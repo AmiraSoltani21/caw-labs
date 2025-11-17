73b346ebee3ada03847be98644f38be6af0d202b
AmiraSoltani21 <amira.soltani@univ-constantine2.dz>
2025-11-17 23:41:33 +0100

    Delete fileimport.test.js

--- fileimport.test.js
+++
@@ -1,8 +0,0 @@
-const { meanOfArray } = require('./fileimport');
-
-describe('fileImport module', () => {
-  test('calculates mean of an array correctly', () => {
-    const arr = [10, 15, 18, 20, 13, 17];
-    expect(meanOfArray(arr)).toBeCloseTo(15.5); 
-  });
-});
